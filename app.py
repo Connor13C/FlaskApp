ba023dad65c8a28079b709fb34d7cb90cde1cc98
Connor13C <connorbcarey@hotmail.com>
2019-01-15 18:58:47 -0700

    more heroku stuff

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, Items
@@ -8,7 +11,7 @@ from resources.store import Store, Stores
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.secret_key = 'encryptionkey'
 api = Api(app)
 
